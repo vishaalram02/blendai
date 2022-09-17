fd83d3ab9e0e42da6006fd282cf0f35c6efec248
Vishaal Ram <vishr@DESKTOP-BNQ2JHU.localdomain>
2022-09-17 16:08:22 -0400

    gRaDiEnT

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,16 +1,42 @@
-import { useState } from 'react';
-import { Group, Text, Container, Title } from '@mantine/core';
+import { useState, useEffect } from 'react';
+import { Group, Text, Container, Title, Center } from '@mantine/core';
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons';
 import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import { useImageStore } from "../hooks/useImageStore";
+import { getCurrentScope } from 'immer/dist/internal';
 
 export function FileUpload () {
+    const [coords, setCoords] = useState({x: 0, y: 0});
+    const [globalCoords, setGlobalCoords] = useState({x: 0, y: 0});
+
+    useEffect(() => {
+        const handleWindowMouseMove = (e: any) => {
+          setGlobalCoords({
+            x: e.screenX,
+            y: e.screenY,
+          });
+        };
+        window.addEventListener('mousemove', handleWindowMouseMove);
+        return () => {
+          window.removeEventListener('mousemove', handleWindowMouseMove);
+        };
+    }, []);
+    const handleMouseMove = (event: any) => {
+        setCoords({
+          x: event.clientX - event.target.offsetLeft,
+          y: event.clientY - event.target.offsetTop,
+        });
+    };
+
     const updateImage = useImageStore(store => store.updateImage);
+
     return (
         <Container size="lg">
-            <Title align="center" size={90} variant="gradient" style={{userSelect: "none", fontFamily: "Nunito Sans"}}>
+            <Center>
+            <Title onMouseMove={handleMouseMove} size={90} variant="gradient" gradient={{from: 'green.2', to: 'green.1', deg: coords.x}} style={{userSelect: "none", fontFamily: "Nunito Sans", margin: 50}}>
                 stabilify
             </Title>
+            </Center>
             <Dropzone
                 onDrop={(files) => updateImage(files[0])}
                 onReject={(files) => console.log('rejected files', files)}
