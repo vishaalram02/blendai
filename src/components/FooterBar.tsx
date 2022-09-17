02b205c44015dbe866d7de89885b144633305ded
anirudh.rahul <anirudhrahul@anjurahul.com>
2022-09-17 16:54:32 -0400

    Update FooterBar.tsx

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Navbar, Center, Tooltip, Group, Button, TextInput, createStyles, Footer } from '@mantine/core';
+import { Navbar, Center, Tooltip, Group, MediaQuery, Button, TextInput, createStyles, Footer, Slider } from '@mantine/core';
 import {
   TablerIcon,
   IconBrush,
@@ -10,12 +10,22 @@ import {
   IconColorPicker,
   IconBucket,
   IconHandStop,
+  IconReload
 } from '@tabler/icons';
 import {ReactComponent as Logo} from '../assets/logo.svg';
 
 const useStyles = createStyles((theme) => ({
     footer: {
         padding: "10px !important"
+    },
+    slider: {
+        width: "150px"
+    },
+    sliderTitle: {
+        fontSize: "1.2em"
+    },
+    hide: {
+        display: "none"
     }
   }),
 );
@@ -29,6 +39,20 @@ export function FooterBar() {
     <Footer height={60} className={classes.footer} p="md" >
 
         <Group position="right">
+
+            <Group>
+                <span className={classes.sliderTitle}> Seed: </span>
+                <Slider
+                    size="lg"
+                    className={classes.slider}
+                    showLabelOnHover={false}
+                    marks={[
+                        { value: 0, label: '0' },
+                        { value: 100, label: '100' },
+                    ]}
+                />
+            </Group>
+
             
             <TextInput
                 placeholder="Your prompt"
