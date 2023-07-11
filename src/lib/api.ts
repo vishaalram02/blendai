f1488ca6cf51dc44140e2f58b2c6e672ea72f730
vishaalram02 <vishaalram02@gmail.com>
2023-07-11 15:21:58 -0400

    blended diffusion args

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,8 @@
-const ngrok = "https://18d0-18-10-156-187.ngrok.io";
 async function postImages(imageb64: string, maskb64: string, prompt: string, seed: number):Promise<string>{
-    const url = ngrok + "/processImage"
-    // console.log("posting image")
+    const url = "/processImage"
     return fetch(url, {
         method: "POST",
         mode: 'cors',
-        // headers: {
-        //     'Content-Type': 'application/json'
-        // },
         body: JSON.stringify({
             baseImage: imageb64,
             maskImage: maskb64,
@@ -20,7 +15,7 @@ async function postImages(imageb64: string, maskb64: string, prompt: string, see
 }
 
 async function checkProgress(url: string):Promise<string>{
-    return fetch(ngrok+url, {mode: 'cors'})
+    return fetch(url, {mode: 'cors'})
     .then((response) => response.text())
 }
 
